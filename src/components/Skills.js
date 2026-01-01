14f2bedd8c9c9b2c7ca8ddb3304f4eb83c4083ee
avinash-sharma-121 <kumaravinashsharma81@gmail.com>
2026-01-01 12:34:41 +0530

    updated skills with github link

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,13 +5,15 @@ const Skills = () => {
             <ul className="skill-list">
                 <li>AWS</li>
                 <li>Docker</li>
-                <li>Kubernetes</li>
-                <li>Jenkins</li>
-                <li>Terraform</li>
-                <li>Ansible</li>
+                <li><a href="https://github.com/avinash-sharma-121/kubernetes_repo" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }} rel="noreferrer">Kubernetes</a></li>
+                <li><a href="https://github.com/avinash-sharma-121/react-testing-with-jenkins" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }} rel="noreferrer">Jenkins</a></li>
+                <li><a href="https://github.com/avinash-sharma-121/terraform_repo" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }} rel="noreferrer">Terraform</a></li>
+                <li><a href="https://github.com/avinash-sharma-121/ansible_repo" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }} rel="noreferrer">Ansible</a></li>
                 <li>Python</li>
                 <li>Linux</li>
-                <li>Git</li>
+                <li><a href="https://github.com/avinash-sharma-121/git_hands_on" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }} rel="noreferrer">Git</a></li>
+                <li>CI/CD</li>
+                <li>Monitoring & Logging</li>
             </ul>
         </section>
     )
