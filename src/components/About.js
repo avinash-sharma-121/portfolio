c74c573d133df448a45f4fb6c3fab86b5a705217
avinash-sharma-121 <kumaravinashsharma81@gmail.com>
2026-02-22 22:39:11 +0530

    updated year of exp

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ const About = () => {
                 </div>
             </div>
             <h2>About Me</h2>
-            <p>AWS Certified DevOps Engineer with 3.8+ years of experience in AWS cloud, Linux Administration, CI/CD, Docker, Kubernetes, Terraform, and automation using Python and Bash. Strong background in building secure, scalable, and automated infrastructure using IaC and DevOps best practices. Experienced in monitoring, patching, and cloud operations. Focused on improving reliability and delivering cloud-native solutions.</p>
+            <p>AWS Certified DevOps Engineer with 4 years of experience in AWS cloud, Linux Administration, CI/CD, Docker, Kubernetes, Terraform, and automation using Python and Bash. Strong background in building secure, scalable, and automated infrastructure using IaC and DevOps best practices. Experienced in monitoring, patching, and cloud operations. Focused on improving reliability and delivering cloud-native solutions.</p>
         </section>
     )
 }
